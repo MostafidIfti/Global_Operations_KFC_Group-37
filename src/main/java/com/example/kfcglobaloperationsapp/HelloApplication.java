ef963172bee556e4aa361c82ebb535bf16b9e680
Mostafidifti41 <2310321@iub.edu.bd>
2026-04-11 18:41:37 +0600

    wowwww

--- src/main/java/com/example/kfcglobaloperationsapp/HelloApplication.java
+++ src/main/java/com/example/kfcglobaloperationsapp/HelloApplication.java
@@ -10,11 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-<<<<<<< HEAD
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/kfcglobaloperationsapp/Uzair_2521139_Fxml_Files/KitchenSupervisor--fxml_files/kitchenSupervisorDashboard.fxml"));
-=======
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/kfcglobaloperationsapp/Ifti_2310321_FXML_FIles/Finance-Fxml_works/FinancialReportDashboard.fxml"));
->>>>>>> adc28288832468c30915e4834f9fca45d520aedf
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Finance Manager Dashboard");
         stage.setScene(scene);
