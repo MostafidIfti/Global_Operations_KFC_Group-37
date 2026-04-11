8f96f4133d60815ed1c406d8621a29a338cce078
KomolaOrange-2521139 <2521139@iub.edu.bd>
2026-04-11 06:32:23 +0600

    bind

---
+++ src/main/java/com/example/kfcglobaloperationsapp/Uzair_2521139/Model/Cashier.java
@@ -0,0 +1,10 @@
+package com.example.kfcglobaloperationsapp.Uzair_2521139;
+
+public class Cashier extends User {
+
+    public Cashier(int id, String name) {
+        super(id, name);
+    }
+
+
+}
